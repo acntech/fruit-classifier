5aefc5b7990cbd81b458352051f1cffb42b1d40b
Fabian Sødal Dietrichson <fabiansd1402@gmail.com>
2019-05-02 16:22:27 +0200

    implemented tests for utils folder. Fixed copytree func

--- fruit_classifier/utils/file_utils.py
+++ fruit_classifier/utils/file_utils.py
@@ -1,11 +1,12 @@
 import os
 import shutil
 from tqdm import tqdm
+from pathlib import Path
 
 
 def copytree(src, dst, symlinks=False, ignore=None):
     """
-    Workaround for shutils weird constraints
+    Copies files from src directory to destination dst
 
     Parameters
     ----------
@@ -23,13 +24,35 @@ def copytree(src, dst, symlinks=False, ignore=None):
     https://stackoverflow.com/questions/1868714/how-do-i-copy-an-entire-directory-of-files-into-an-existing-directory-using-pyth
     """
 
-    src = str(src)
-    dst = str(dst)
+    src = Path(src)
+    dst = Path(dst)
 
     for item in tqdm(os.listdir(src), desc='Copying files'):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
+        s = src.joinpath(item)
+        d = dst.joinpath(item)
+
+        if s.is_dir() and not d.exists():
+            shutil.copytree(str(s), str(d), symlinks, ignore)
+        elif not d.exists():
+            shutil.copy2(str(s), str(d))
         else:
-            shutil.copy2(s, d)
+            pass
+
+    print(os.listdir(src))
+    print(os.listdir(dst))
+
+
+if __name__ == '__main__':
+    #Testing the copytree function
+
+    root_dir = Path(__file__).absolute().parents[1]
+
+    src_path = root_dir.joinpath('data_scraping')
+    dst_path = root_dir.joinpath('data_scraping_copy')
+
+    if not dst_path.is_dir():
+        dst_path.mkdir(parents=True, exist_ok=True)
+
+    copytree(src=src_path, dst=dst_path)
+
+    shutil.rmtree(dst_path)
---
+++ test/utils/test_file_utils.py
@@ -0,0 +1,38 @@
+import unittest
+from pathlib import Path
+import shutil
+from fruit_classifier.utils.file_utils import copytree
+import os
+
+
+class TestFileUtils(unittest.TestCase):
+
+    def setUp(self):
+        self.root_dir = Path(__file__).absolute().parents[1]
+        self.src_path = self.root_dir.joinpath('test_data')
+        self.dst_path = self.root_dir.joinpath('test_data_copy')
+
+        if not self.dst_path.is_dir():
+            self.dst_path.mkdir(parents=True, exist_ok=True)
+
+    def test_copytree(self):
+        copytree(self.src_path, self.dst_path)
+
+        self.assertTrue(self.dst_path.is_dir(), 'The copied files does not exist')
+
+        src_files = os.listdir(self.src_path)
+        dst_files = os.listdir(self.dst_path)
+
+        for i, item in enumerate(src_files):
+            self.assertEqual(dst_files[i], item,
+                            msg='Copied files are not matching\n\n ' 
+                            f'{dst_files[i]} \n'
+                            f'{item}')
+
+    def tearDown(self):
+        shutil.rmtree(self.dst_path)
+        self.assertFalse(self.dst_path.is_dir(), 'The copied files was not deleted')
+
+
+if __name__ == '__main__':
+    unittest.main()
