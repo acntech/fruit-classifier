9bad72401986e979f401c442e02669d9b09203a7
Tord B. Brandsæter <tord.b.brandsaeter@accenture.com>
2019-05-06 14:48:50 +0200

    Added more tests to the file.

--- test/train/test_train_utils.py
+++ test/train/test_train_utils.py
@@ -1,6 +1,7 @@
 import unittest
 from fruit_classifier.train.train_utils import get_image_paths
 from fruit_classifier.train.train_utils import get_data_and_labels
+from fruit_classifier.train.train_utils import get_model_input
 from pathlib import Path
 import shutil
 
@@ -8,16 +9,20 @@ import shutil
 class TestTrainUtils(unittest.TestCase):
     def setUp(self):
         test_dir = Path(__file__).absolute().parents[1]
+
+        # Select a unique folder name for a new folder
         folder_name = 'temp_folder'
         self.folderName = test_dir.joinpath(folder_name)
         while self.folderName.is_dir():
             folder_name = folder_name + '_'
             self.folderName = test_dir.joinpath(folder_name)
 
+        # Make directory and two subdirectories
         self.folderName.mkdir(parents=True, exist_ok=True)
         self.folderName.joinpath('A').mkdir(parents=True, exist_ok=True)
         self.folderName.joinpath('B').mkdir(parents=True, exist_ok=True)
 
+        # Copy an image into each of those subdirectories
         orig_file_path = test_dir.joinpath("test_data").\
             joinpath("original_test_image.jpg")
         shutil.copy(str(orig_file_path),
@@ -25,10 +30,23 @@ class TestTrainUtils(unittest.TestCase):
         shutil.copy(str(orig_file_path),
                     str(self.folderName.joinpath('B')))
 
-    def test_not_crashing(self):
+    def test_does_not_crash(self):
+        # Test get_image_paths
         image_paths = get_image_paths(self.folderName)
         self.assertGreater(len(image_paths), 0)
+
+        # Test get_data_and_labels
         data, labels = get_data_and_labels(image_paths)
+        self.assertGreater(len(data), 0)
+        self.assertGreater(len(labels), 0)
+
+        # Test get_model_input
+        x_train, x_val, y_train, y_val = get_model_input(data, labels)
+        self.assertEqual(1, len(x_train))
+        self.assertEqual(1, len(x_val))
+        self.assertEqual(1, len(y_train))
+        self.assertEqual(1, len(y_val))
+
 
     def tearDown(self):
         folder_to_remove = self.folderName
