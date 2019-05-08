37f48e8432f9d4fe8e848dfea490d0a0937a2020
Tord B. Brandsæter <tord.b.brandsaeter@accenture.com>
2019-05-08 11:02:09 +0200

    Gathered all 6 tests and training in 1 test +fixes
    To avoid setUp from running the same training 6 times, the training was
    gathered in one single test function. And since we otherwise cannot
    store the outputs of the training for other tests to verify, I also
    moved all the output verification into that function. Finally, I reduced
    from training 25 epochs to 1 epoch.

--- test/train/test_train_utils.py
+++ test/train/test_train_utils.py
@@ -35,73 +35,51 @@ class TestTrainUtils(unittest.TestCase):
         shutil.copy(str(orig_file_path),
                     str(self.folder_name.joinpath('B')))
 
-        # Run get_image_paths()
+    def test_train_model(self):
+        # Run get_image_paths() and verify outputs
         image_paths = get_image_paths(self.folder_name)
-        self.num_image_paths = len(image_paths)
+        num_image_paths = len(image_paths)
+        self.assertGreater(num_image_paths, 0)
 
-        # Run get_data_and_labels
+        # Run get_data_and_labels and verify outputs
         data, labels = get_data_and_labels(image_paths)
-        self.num_data = len(data)
-        self.num_labels = len(labels)
+        self.assertGreater(len(data), 0)
+        self.assertGreater(len(labels), 0)
 
-        # Run get_model_input
+        # Run get_model_input and verify outputs
         x_train, x_val, y_train, y_val = get_model_input(data, labels)
-        self.num_x_train = len(x_train)
-        self.num_x_val = len(x_val)
-        self.num_y_train = len(y_train)
-        self.num_y_val = len(y_val)
+        self.assertEqual(1, len(x_train))
+        self.assertEqual(1, len(x_val))
+        self.assertEqual(1, len(y_train))
+        self.assertEqual(1, len(y_val))
 
-        # Run get_image_generator
-        self.image_generator = get_image_generator()
+        # Run get_image_generator and verify outputs
+        image_generator = get_image_generator()
+        self.assertEqual(image_generator.rotation_range, 30)
+        self.assertEqual(image_generator.width_shift_range, .1)
+        self.assertEqual(image_generator.height_shift_range, .1)
+        self.assertEqual(image_generator.shear_range, .2)
+        self.assertEqual(image_generator.horizontal_flip, True)
+        self.assertEqual(image_generator.fill_mode, 'nearest')
 
-        # Run get_model
-        model = get_model(len(set(labels)), epochs=1)
-        self.model_was_built = model._built
+        # Run get_model and verify outputs
+        num_intended_epochs = 1
+        model = get_model(len(set(labels)), epochs=num_intended_epochs)
+        self.assertTrue(model._built)
 
-        # Run train_model
+        # Run train_model and verify outputs
         history = train_model(model,
-                              self.image_generator,
+                              image_generator,
                               x_train,
                               y_train,
                               x_val,
-                              y_val)
-        self.num_epochs = history.params['epochs']
+                              y_val,
+                              epochs=num_intended_epochs)
+        num_epochs = history.params['epochs']
+        self.assertEqual(num_epochs, 1)
 
-        # Run plot_training
+        # Run plot_training and verify it does not crash
         plot_training(history)
-
-    def test_get_image_paths(self):
-        # Test get_image_paths output
-        self.assertGreater(self.num_image_paths, 0)
-
-    def test_get_data_and_labels(self):
-        # Test get_data_and_labels output
-        self.assertGreater(self.num_data, 0)
-        self.assertGreater(self.num_labels, 0)
-
-    def test_get_model_input(self):
-        # Test get_model_input output
-        self.assertEqual(1, self.num_x_train)
-        self.assertEqual(1, self.num_x_val)
-        self.assertEqual(1, self.num_y_train)
-        self.assertEqual(1, self.num_y_val)
-
-    def test_image_generator(self):
-        # Test get_image_generator output
-        self.assertEqual(self.image_generator.rotation_range, 30)
-        self.assertEqual(self.image_generator.width_shift_range, .1)
-        self.assertEqual(self.image_generator.height_shift_range, .1)
-        self.assertEqual(self.image_generator.shear_range, .2)
-        self.assertEqual(self.image_generator.horizontal_flip, True)
-        self.assertEqual(self.image_generator.fill_mode, 'nearest')
-
-    def test_model(self):
-        # Test get_model output
-        self.assertTrue(self.model_was_built)
-
-    def test_train_model(self):
-        # Test train_model output
-        self.assertEqual(self.num_epochs, 25)
 
     def tearDown(self):
         # Delete the temporary folder its contents
