5ab6cf59d3b5f2b3bd3d7018935f7775acaa1042
Tord B. Brandsæter <tord.b.brandsaeter@accenture.com>
2019-05-15 13:01:12 +0200

    Background behind text when printing text on img
    When displaying an image with text written upon it, the text is now
    black with a white background surrounding it.

--- fruit_classifier/predict/predict_utils.py
+++ fruit_classifier/predict/predict_utils.py
@@ -6,6 +6,51 @@ from keras.engine.saving import load_model
 from skimage.transform import resize
 
 
+def draw_text_with_background(output_image, text="text",
+                              text_offset_x=10, text_offset_y=25,
+                              font_scale=.7, font_thickness=2,
+                              font_color=(0, 0, 0),
+                              font_bg_color=(255, 255, 255),
+                              font=cv2.FONT_HERSHEY_SIMPLEX,
+                              padding=5):
+    """
+    Draws text inside a colored box ontop of an image
+
+    Parameters
+    ----------
+    output_image    : cv2 image -  The image
+    text            : str - The text
+    text_offset_x   : integer - text start position x axis
+    text_offset_y   : integer - text start position y axis
+    font_scale      : integer - font scale used
+    font_thickness  : integer - font thickness used
+    font_color      : integer array - text color
+    font_bg_color   : integer array - background box color
+    font            : cv2 font - font of text
+    padding         : integer - margin of the box around text
+    """
+
+    # get the width and height of the text box from the text
+    (text_width, text_height) = \
+        cv2.getTextSize(text, font, fontScale=font_scale,
+                        thickness=font_thickness)[0]
+
+    # make the coords of the box with a small padding
+    box_coords = ((text_offset_x - padding,
+                   text_offset_y + padding),
+                  (text_offset_x + (text_width + padding),
+                   text_offset_y - (text_height + padding)))
+
+    # Draw the background box onto the image variable
+    cv2.rectangle(output_image, box_coords[0], box_coords[1],
+                  font_bg_color, cv2.FILLED)
+
+    # Draw the text onto the image variable and display it
+    cv2.putText(output_image, text, (text_offset_x, text_offset_y),
+                font, fontScale=font_scale, color=font_color,
+                thickness=font_thickness)
+
+
 def draw_class_on_image(image, probability_text):
     """
     Draws the class and confidence on the image
@@ -31,13 +76,7 @@ def draw_class_on_image(image, probability_text):
                           mode='reflect',
                           anti_aliasing=True).astype(np.uint8)
 
-    cv2.putText(output_image,
-                probability_text,
-                (10, 25),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.7,
-                (0, 255, 0),
-                2)
+    draw_text_with_background(output_image, probability_text)
 
     return output_image
 
