f4458e6490c6e13077df3fbc8d1aa57391938b46
loeiten <michael.l.magnussen@gmail.com>
2019-09-26 10:52:16 +0200

    Bugfixes to deploy

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-cd ..
-echo $PWD
 docker build -f docker/Dockerfile -t loeiten/fruit_classifier:latest .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker docker push loeiten/fruit_classifier:latest
